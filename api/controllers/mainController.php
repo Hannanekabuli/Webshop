f363427aa074586c8b19ef1dc9136c6c789d063c
donyateimouri <donyateimouri1@gmail.com>
2022-03-02 13:54:30 +0100

    Update mainController.php
    main update

--- api/controllers/mainController.php
+++ api/controllers/mainController.php
@@ -1,1 +1,20 @@
 
+<?php
+include_once(dirname(__DIR__)."/class/database.php");
+
+abstract class MainController {
+
+    public $database;
+
+    function __construct($table, $class) {
+        $this->database = new Database($table, $class);
+    }
+
+    abstract function getAll();
+    abstract function getById($id);
+    abstract function add($product);
+    abstract function delete($id);
+    
+}
+
+?>
