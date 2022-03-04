68356bb03c20b8b526f98648d3c85305a77973cd
Niki-van <hanko.vangjeli@studio.unibo.it>
2022-03-04 10:49:41 +0100

    Update dashboard.php
    ok

---
+++ customer /dashboard.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../sections/header.php";
+
+if (!isset($_COOKIE['Customer-Login'])){
+    header('location:login.php');
+}
+?>
+
+
+
+
+
+
+<?php require_once "../sections/footer.php";?>
