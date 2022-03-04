ef27298eaf5d56938ea0c453497ebc3d95bcbd67
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-03-04 13:58:25 +0100

    productController Update
    controller

--- api/controller/productController.php
+++ api/controller/productController.php
@@ -1,11 +1,11 @@
 <?php
-$base_root = $_SERVER['DOCUMENT_ROOT']."/NyaWebshop 2/Webshop";
-include_once($base_root."/classes/createInstanceFunctions.php");
-include_once($base_root."/controllers/mainController.php");
+
+include_once(dirname(__DIR__)."/class/createInstanceFunctions.php");
+include_once(dirname(__DIR__)."/controller/mainController.php");
 
 class ProductController extends MainController {
 
-    private $createFunction = "createProduct";
+    private $createFunction = "createOrder";
 
     function __construct() {
         parent::__construct("Products", "Product");
@@ -38,7 +38,8 @@ class ProductController extends MainController {
         return $this->database->freeQuery("UPDATE products set quantity = $quantity where id = $id");
     }
     public function category_products(){
-        $categories = $this->database->freeQuery("SELECT * from categories group by name");
+        $categories = $this->database->freeQuery("SELECT * from categories");
+
         $result = [];
         foreach ($categories as $category){
             $category_products = $this->database->freeQuery("SELECT * FROM categorydetails where categoryID=".$category['ID']);
@@ -48,7 +49,7 @@ class ProductController extends MainController {
             // Get Category Products
             while ($i < count($category_products)){
                 $product_id = $category_products[$i]["productID"];
-                $products[$i] =  $this->database->freeQuery("SELECT `ID`, `name`, `quantity`, `price`, `description` FROM products where ID = ".$product_id)[0];
+                $products[$i] =  $this->database->freeQuery("SELECT `ID`, `name`, `quantity`, `price`, `description`, `image` FROM products where ID = $product_id limit 2")[0];
                 $i++;
             }
             $result[$category['ID']] = [
