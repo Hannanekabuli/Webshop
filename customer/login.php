d536ac3236bdcc0c891fd80d08699f94fb4a8167
Niki-van <hanko.vangjeli@studio.unibo.it>
2022-03-04 13:17:02 +0100

    Update login.php
    login ok

--- customer/login.php
+++ customer/login.php
@@ -11,28 +11,49 @@ if (isset($_COOKIE['Customer-Login'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camera Webshop | Category</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >-->
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/product.css">
-    <link rel="stylesheet" href="assets/css/startpage.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
+">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/product.css">
+    <link rel="stylesheet" href="../assets/css/startpage.css">
     <!--    <script defer type="module" src="assets/js/index.js"></script>-->
 </head>
 <body>
-<header>
+    <header>
+
     <div class="header">
-        <div class="menu-bar">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid" style="background-color: rgb(200, 196, 201); height: 90px; margin-top: -10px;">
-                    <a href="index.php">
-                        <h3 class="float-md-start mb-0">ReflexMania</h3>
-                    </a>
-                    <ul class="navbar-nav justify-content-end">
-                        <ul class="navbar-nav justify-content-end" >
-                            <li class="nav-item">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" style="color:rgb(20, 29, 155);" href="../index.php" >ReflexMania</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav" style="padding-right:20px">
+      <ul class="nav navbar-nav ml-auto"> 
+                            <!--                        <li class="nav-item">-->
+                            <!--                            <div class="dropdown">-->
+                            <!--                                <button class="btn btn-white dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"  style="font-size: 1rem; color: black;">-->
+                            <!--                                    Category-->
+                            <!--                                </button>-->
+                            <!--                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">-->
+                            <!--                                    <li><a class="dropdown-item" href="#">Action Camera</a></li>-->
+                            <!--                                    <li><a class="dropdown-item" href="#">Analog Camera</a></li>-->
+                            <!--                                    <li><a class="dropdown-item" href="#">Compact Camera</a></li>-->
+                            <!--                                    <li><a class="dropdown-item" href="#">System Camera</a></li>-->
+                            <!--                                </ul>-->
+                            <!--                            </div>-->
+                            <!--                        </li>-->
+
+<!--                         <li class="nav-item ">
+                         <a class="nav-link" style="font-size: 1rem; color:black" href="../category.php">Category<i class="fa fa-camera-retro fa-lg" style="font-size: 1rem;"></i></a>
+                        </li> -->
+                                <li class="nav-item">
+                                <a class="nav-link" href=""  style="font-size: 1rem; color: black;">
                                 <?php
                                 if (isset($_COOKIE['Customer-Login'])){
                                     echo '<button class="btn btn-link" onclick="logout()"  style="font-size: 1rem; color: black;"> Logout <i class="bi bi-person"></i></button>';
@@ -40,58 +61,57 @@ if (isset($_COOKIE['Customer-Login'])){
                                     echo '<a class="nav-link" href="../customer/login.php"  style="font-size: 1rem; color: black;"> Login <i class="bi bi-person"></i></a>';
                                 }
                                 ?>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../aboutus.html"  style="font-size: 1rem; color: black;">About us<i class="bi bi-question-circle"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../cart.php"  style="font-size: 1rem; color: black;">
-                                    Cart
-                                    <i class="bi bi-cart"></i>
-                                    <span id="cartCount" class="badge rounded-pill bg-danger"></span>
                                 </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../checkout.php"  style="font-size: 1rem; color: black;"> Checkout <i class="bi bi-person"></i></a>
-                            </li>
-
-                        </ul>
-            </nav>
+                                </li>
+                                
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../aboutus.html"  style="font-size: 1rem; color: black;">About us<i class="bi bi-question-circle"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../cart.php"  style="font-size: 1rem; color: black;"> Cart <i class="bi bi-person"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" > 
+                                <i class="bi bi-cart"></i>
+                                <span id="cartCount" class="badge rounded-pill bg-danger"></span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../checkout.php"  style="font-size: 1rem; color: black;">Checkout<i class="bi bi-person"></i></a>
+                                </li>
+                            </ul>
+                </nav>
+            </div>
         </div>
+    </header>
+    <div class="w-50 mx-auto border p-2 rounded mt-5">
+        <h4 class="font-weight-bold mt-2 mb-2">Customer Login</h4>
+
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+            </div>
+
+        </form>
+        <br>
+        <button type="submit" class="btn btn-primary" onclick="checkLogin()">Login</button>
+        <a href="register.php" class="btn btn-outline-primary">Register</a>
+
     </div>
-</header>
+    <script src="../assets/js/main.js"></script>
 
+    <script>
+        async function checkLogin() {
+            var body = new FormData()
+            body.append("action", "login")
+            body.append("email", document.getElementById('email').value)
+            body.append("password", document.getElementById('password').value)
 
-
-
-<div class="w-50 mx-auto border p-2 rounded mt-5">
-    <h4 class="font-weight-bold mt-2 mb-2">Customer Login</h4>
-
-    <form id="login_form">
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-        </div>
-
-    </form>
-    <br>
-    <button type="submit" class="btn btn-primary" onclick="checkLogin()">Login</button>
-    <a href="register.php" class="btn btn-outline-primary">Register</a>
-
-</div>
-<script src="../assets/js/main.js"></script>
-
-<script>
-    async function checkLogin() {
-        var body = new FormData()
-        body.append("action", "login")
-        body.append("email", document.getElementById('email').value)
-        body.append("password", document.getElementById('password').value)
-        if (checkForm('login_form')){
+            if (checkForm('login_form')){
             let status = await makeRequest(`../api/receiver/customerReceiver.php`, "POST", body)
             if (status){
                 window.location.href = "../category.php";
@@ -99,9 +119,24 @@ if (isset($_COOKIE['Customer-Login'])){
                 alert('Email or Password is invalid!');
             }
         }
-
-    }
-</script>
-
+        }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
